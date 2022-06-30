0a81f8d3093af6dc605c84a016149d1b9b0a861b
devcarloshenrique <devcarloshenrique@gmail.com>
2022-06-29 22:37:14 -0300

    refactor: importing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hellow World</h1>      
-    </div>
+    
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle/>
+    </>
+    
   );
 }
