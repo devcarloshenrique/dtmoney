485769c5683b0b84bb552b4d9b03f9c38e26eaee
devcarloshenrique <devcarloshenrique@gmail.com>
2022-07-02 11:36:39 -0300

    refactor: global style model

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './style';
 
 interface NewTransactionsModalProps {
     isOpen: boolean;
@@ -10,8 +11,30 @@ export function NewTransactionsModal({ isOpen, onRequestClose } : NewTransaction
         <Modal 
         isOpen={isOpen} 
         onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
       >
-        <h2>Cadastrar</h2>
+        <Container>
+          <h2>Cadastrar transação</h2>
+
+          <input 
+            placeholder='Titulo'
+          />
+          
+          <input 
+            type='number'
+            placeholder='Valor'
+          />
+
+          <input 
+            placeholder='Categoria'
+          />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+          
+        </Container>
       </Modal>
     )
 }
