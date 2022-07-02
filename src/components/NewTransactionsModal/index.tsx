77ac57803ed92d28546e20312e6f1edf95a1ce59
devcarloshenrique <devcarloshenrique@gmail.com>
2022-07-02 14:08:34 -0300

    refactor: stylized modal type buttons

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,7 +1,10 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/fechar.svg';
+import incomeImg from '../../assets/entradas.svg';
+import outcomeImg from '../../assets/saidas.svg';
 
-import { Container } from './style';
+
+import { Container, TransactionTypeContainer } from './style';
 
 interface NewTransactionsModalProps {
     isOpen: boolean;
@@ -39,6 +42,22 @@ export function NewTransactionsModal({ isOpen, onRequestClose } : NewTransaction
           <input 
             placeholder='Categoria'
           />
+
+          <TransactionTypeContainer>
+            <button 
+              type="button"
+            >
+              <img src={incomeImg} alt="Entradas" />
+              <span>Entrada</span>
+            </button>
+
+            <button 
+              type="button"
+            >
+              <img src={outcomeImg} alt="Saída" />
+              <span>Saída</span>
+            </button>
+          </TransactionTypeContainer>
 
           <button type="submit">
             Cadastrar
