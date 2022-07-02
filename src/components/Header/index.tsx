3164ca13dc01c12f14dbac3534abd1df9a85d5b4
devcarloshenrique <devcarloshenrique@gmail.com>
2022-07-02 10:50:04 -0300

    refactor: creating modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,21 @@ import logo from '../../assets/logo.svg';
 import { Container } from './styles';
 import { Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal } : HeaderProps ) {
+
     return (
         <Container>
             <Content>
             <img src={logo} alt="dt money" />
-            <button type="button">
+            <button type="button" onClick={onOpenNewTransactionModal}>
                 Nova transação
             </button>
+
+
             </Content>
         </Container>
     )
