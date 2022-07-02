4f463e9fbfabd21a0f432fcf5ef77b81884952c4
devcarloshenrique <devcarloshenrique@gmail.com>
2022-07-02 11:16:35 -0300

    feat: creating component NewTransactionsModal

---
+++ src/components/NewTransactionsModal/index.tsx
@@ -0,0 +1,17 @@
+import Modal from 'react-modal';
+
+interface NewTransactionsModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionsModal({ isOpen, onRequestClose } : NewTransactionsModalProps ) {
+    return (
+        <Modal 
+        isOpen={isOpen} 
+        onRequestClose={onRequestClose}
+      >
+        <h2>Cadastrar</h2>
+      </Modal>
+    )
+}
