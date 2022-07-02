c78c3c515c6080f62416fe8f828ce383a9062d6b
devcarloshenrique <devcarloshenrique@gmail.com>
2022-07-02 16:08:44 -0300

    refactor: request post route transactions

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,11 +1,12 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
+
 import closeImg from '../../assets/fechar.svg';
 import incomeImg from '../../assets/entradas.svg';
 import outcomeImg from '../../assets/saidas.svg';
-
+import { api } from '../../services/api';
 
 import { Container, TransactionTypeContainer, RadioBox } from './style';
-import { FormEvent, useState } from 'react';
 
 interface NewTransactionsModalProps {
     isOpen: boolean;
@@ -13,18 +14,25 @@ interface NewTransactionsModalProps {
 }
 
 export function NewTransactionsModal({ isOpen, onRequestClose } : NewTransactionsModalProps ) {
-
   
   const [title, setTitle] = useState('');
   const [value, setValue] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');  
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  function handleCreateNewTransaction(event: FormEvent) {    
     event.preventDefault();
+    
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
+
+    api.post('/transactions', data);
+
   }
-
-
 
   return (
       <Modal 
